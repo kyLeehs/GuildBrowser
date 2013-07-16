a9b3c378caa1ec2d608d91dff1352204ead904e6
Lee hyunseok <leehyunseok@Lee-ui-iMac.local>
2013-07-16 16:20:15 +0900

    add unit test

---
+++ GuildBrowserLogicTests/CharacterTests.h
@@ -0,0 +1,13 @@
+//
+//  CharacterTests.h
+//  GuildBrowser
+//
+//  Created by Lee hyunseok on 13. 7. 16..
+//  Copyright (c) 2013년 Charlie Fulton. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface CharacterTests : SenTestCase
+
+@end
