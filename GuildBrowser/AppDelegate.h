1501ab702311154f978b4ec0a75bc49be3462feb
kyLeehs <hslee82@gmail.com>
2013-09-06 17:05:27 +0900

    add scripts

--- GuildBrowser/AppDelegate.h
+++ GuildBrowser/AppDelegate.h
@@ -1,5 +1,10 @@
 #import <UIKit/UIKit.h>
 
+#ifdef DEBUG
+FILE *fopen$UNIX2003(const char *filename, const char *mode);
+size_t fwrite$UNIX2003(const void *ptr, size_t size, size_t nitems, FILE *stream);
+#endif
+
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
